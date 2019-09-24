f4773981d6e1a7c939db72456db7430f29cab795
blothe <b.lothe@codeur.online>
2019-09-24 23:10:03 +0200

    Tutorial content : test-examples.php ex2

--- test-examples.php
+++ test-examples.php
@@ -15,16 +15,29 @@ catch(PDOException $e)
 }
 
 ?>
-<h3>Liste des personnes dont le nom de famille est Palmer :</h3>
+<h3 style="text-decoration:underline">Liste des personnes dont le nom de famille est Palmer :</h3>
+<p style="text-align:justify">
 <?php
 $stmt = $conn->query("SELECT last_name, first_name FROM `data` WHERE last_name = 'Palmer'");
 while ($result = $stmt->fetch())
 {
-  echo "<li>" . $result['last_name'] . " " . $result['first_name'] . "</li>";
+  echo "<span>" . $result['last_name'] . " " . $result['first_name'] . " | </span>";
 }
+echo "TERMINÉ";
 ?>
+</p>
 
-
+<h3 style="text-decoration:underline">Liste des femmes :</h3>
+<p style="text-align:justify">
+<?php
+$stmt = $conn->query("SELECT last_name, first_name FROM `data` WHERE gender = 'female'");
+while ($result = $stmt->fetch())
+{
+  echo "<span>" . $result['last_name'] . " " . $result['first_name'] . " | </span>";
+}
+echo "TERMINÉ";
+?>
+</p>
 
 <?php
 // close the connection
