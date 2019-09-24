ef4c854fc014a727efbfd3cd2894ceae83e49a75
blothe <b.lothe@codeur.online>
2019-09-24 22:49:58 +0200

    Tutorial content : test-examples.php ex1

--- test-examples.php
+++ test-examples.php
@@ -14,6 +14,19 @@ catch(PDOException $e)
   echo "Connection failed: " . $e->getMessage();
 }
 
+?>
+<h3>Liste des personnes dont le nom de famille est Palmer :</h3>
+<?php
+$stmt = $conn->query("SELECT last_name, first_name FROM `data` WHERE last_name = 'Palmer'");
+while ($result = $stmt->fetch())
+{
+  echo "<li>" . $result['last_name'] . " " . $result['first_name'] . "</li>";
+}
+?>
+
+
+
+<?php
 // close the connection
 $conn = null;
 ?>
